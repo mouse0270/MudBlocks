423be38b57a789a9f22b2f3053385c3ecbfba64b
Robert McIntosh <rmcintosh@remabledesigns.com>
2024-06-28 10:42:50 -0400

    Attempt 4

--- MudBlocks/obj/Debug/net8.0/MudBlocks.AssemblyInfo.cs
+++ MudBlocks/obj/Debug/net8.0/MudBlocks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MudBlocks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a8da8e69660a3b3990a9619b3a28445854ba433")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97d9da0b275e5978c3e24473e49bb56b1b915c85")]
 [assembly: System.Reflection.AssemblyProductAttribute("MudBlocks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MudBlocks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
